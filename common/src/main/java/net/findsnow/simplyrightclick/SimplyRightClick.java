2671f4e529320fda012a8c2b39e9e77bd5e745d6
findsnow <okaytrismail@gmail.com>
2025-03-30 22:25:18 -0500

    1.21.1

---
+++ common/src/main/java/net/findsnow/simplyrightclick/SimplyRightClick.java
@@ -0,0 +1,9 @@
+package net.findsnow.simplyrightclick;
+
+public final class SimplyRightClick {
+	public static final String MOD_ID = "simplyrightclick";
+
+	public static void init() {
+		// Write common init code here.
+	}
+}
---
+++ neoforge/src/main/java/net/findsnow/simplyrightclick/neoforge/SimplyRightClickNeoForge.java
@@ -0,0 +1,12 @@
+package net.findsnow.simplyrightclick.neoforge;
+
+import net.findsnow.simplyrightclick.SimplyRightClick;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SimplyRightClick.MOD_ID)
+public final class SimplyRightClickNeoForge {
+	public SimplyRightClickNeoForge() {
+		// Run our common setup.
+		SimplyRightClick.init();
+	}
+}
